03228ee959063e17c0782ba1a19e9bc6b7f1ba7a
aaron <aaron@pop-os.localdomain>
2022-05-05 13:02:50 -0400

    backend accepts input, detects duplicates, validates range

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, Form, Request
 from fastapi.templating import Jinja2Templates
 
+from quiz import find_majority, check_for_duplicates
+
 app = FastAPI()
 templates = Jinja2Templates(directory='../templates/')
 
@@ -16,13 +18,49 @@ async def root(request: Request):
     )
 
 @app.post('/')
-async def root(request: Request, color: str = Form(...)):
-    result = color
+async def root(
+        request: Request, 
+        r0c0: int = Form(...),
+        r0c1: int = Form(...),
+        r0c2: int = Form(...),
+        r0c3: int = Form(...),
+        r1c0: int = Form(...),
+        r1c1: int = Form(...),
+        r1c2: int = Form(...),
+        r1c3: int = Form(...),
+        r2c0: int = Form(...),
+        r2c1: int = Form(...),
+        r2c2: int = Form(...),
+        r2c3: int = Form(...),
+        r3c0: int = Form(...),
+        r3c1: int = Form(...),
+        r3c2: int = Form(...),
+        r3c3: int = Form(...),
+):
+    Rankings = [
+            [r0c0, r0c1, r0c2, r0c3],
+            [r1c0, r1c1, r1c2, r1c3],
+            [r2c0, r2c1, r2c2, r2c3],
+            [r3c0, r3c1, r3c2, r3c3]
+    ]
+
+    if check_for_duplicates(Rankings):
+        return templates.TemplateResponse(
+                'quiz.html', 
+                context={
+                    'request': request,
+                    'result': None
+                }
+        )
+
+    colors, totals = find_majority(Rankings)
+    result = {'colors': colors, 'totals': totals, 'rankings': Rankings}
+
     return templates.TemplateResponse(
             'result.html', 
             context={
                 'request': request,
-                'result': result
+                'result': result,
             }
     )
 
---
+++ app/quiz.py
@@ -0,0 +1,25 @@
+# map color to number
+C = {
+        0 : 'orange',
+        1 : 'yellow',
+        2 : 'blue',
+        3 : 'green'
+}
+
+# 2D array to answer
+def find_majority(Ranks: list) -> (list, list):
+    # find totals
+    Totals = [0, 0, 0, 0]
+    for row in range(len(Ranks)):
+        for col in range(len(Ranks[0])):
+            Totals[col] += Ranks[row][col]
+    # find majority
+    ans = []
+    for i, total in enumerate(Totals):
+        if total == max(Totals):
+            ans.append(C[i])
+    return (ans, Totals)
+
+def check_for_duplicates(Ranks: list) -> bool:
+    return not all([len(set(row)) == len(row) for row in Ranks])
+
